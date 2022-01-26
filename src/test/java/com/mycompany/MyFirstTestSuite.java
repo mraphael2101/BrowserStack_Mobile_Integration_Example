a82eb3933c9309b352ac03696e23fd2dc09e6cd8
Raphael <RAPHAM@uk.softlab.net>
2022-01-26 10:48:08 +0000

    Initial commit

---
+++ src/test/java/com/mycompany/MyFirstTestSuite.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyFirstTestSuite {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+
+}
